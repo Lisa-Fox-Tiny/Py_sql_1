6a96cc5d9644876a76cd077a3d8e73fcc6ad64a6
Елизавета Столбова <LisaTiny@yandex.ru>
2023-08-15 17:12:06 +0300

    Modification

--- main.py
+++ main.py
@@ -55,14 +55,21 @@ def add_client(cur, first_name, last_name, email, phones=None):
 
 def change_client(cur, client_id, first_name=None, last_name=None, email=None):
     cur.execute("""
-        UPDATE clients
-        SET first_name = %s, last_name = %s, email = %s
-        where id = %s;
-        """, (first_name, last_name, email, client_id))
-    cur.execute("""
-        SELECT * FROM clients;
+        SELECT * FROM clients
         """)
-    return cur.fetchall()
+    index = cur.fetchone()
+    if first_name is None:
+        first_name = index[1]
+    if last_name is None:
+        last_name = index[2]
+    if email is None:
+        email = index[3]
+    cur.execute("""
+            UPDATE clients
+            SET first_name = %s, last_name = %s, email = %s
+            where id = %s;
+            """, (first_name, last_name, email, client_id))
+    return client_id
 
 
 def delete_phone(cur, phone):
@@ -86,53 +93,75 @@ def delete_client(cur, id):
 
 
 def find_client(cur, first_name=None, last_name=None, email=None, phones=None):
-    cur.execute("""
-        SELECT c.id, c.first_name, c.last_name, c.email, p.phone FROM clients c
-        LEFT JOIN phone_numbers p ON c.id = p.client_id
-        WHERE c.first_name=%s OR c.last_name=%s OR c.email=%s OR p.phone=%s;
-        """, (first_name, last_name, email, phones))
+    if first_name is None:
+        first_name = '%'
+    else:
+        first_name = '%' + first_name + '%'
+    if last_name is None:
+        last_name = '%'
+    else:
+        last_name = '%' + last_name + '%'
+    if email is None:
+        email = '%'
+    else:
+        email = '%' + email + '%'
+    if phones is None:
+        cur.execute("""
+                SELECT c.id, c.first_name, c.last_name, c.email, p.phone FROM clients c
+                LEFT JOIN phone_numbers p ON c.id = p.client_id
+                WHERE c.first_name LIKE %s AND c.last_name LIKE %s AND c.email LIKE %s
+                """, (first_name, last_name, email))
+    else:
+        cur.execute("""
+                SELECT c.id, c.first_name, c.last_name, c.email, p.phone FROM clients c
+                LEFT JOIN phone_numbers p ON c.id = p.client_id
+                WHERE c.first_name LIKE %s AND c.last_name LIKE %s AND c.email LIKE %s AND p.phone LIKE %s
+                """, (first_name, last_name, email, phones))
     return cur.fetchall()
 
 
-with psycopg2.connect(database='base', user='postgres', password='123456912') as conn:
-    with conn.cursor() as curs:
-        drop_db(curs)
-        create_db(curs)
-        add_client(curs, "Михаил", "Шабалин", "712qfd@gmail.com")
-        add_client(curs, "Кирилл", "Лебедев", "009aaf@gmail.com")
-        curs.execute("""
-            SELECT c.id, c.first_name, c.last_name, c.email, p.phone FROM clients c
-            LEFT JOIN phone_numbers p ON c.id = p.client_id
-            ORDER BY c.id
-        """)
-        print(curs.fetchall())
-        add_phone(curs, 1, 79163452130)
-        curs.execute("""
-                    SELECT c.id, c.first_name, c.last_name, c.email, p.phone FROM clients c
-                    LEFT JOIN phone_numbers p ON c.id = p.client_id
-                    ORDER BY c.id
+if __name__ == "__main__":
+    with psycopg2.connect(database='base', user='postgres', password='123456912') as conn:
+        with conn.cursor() as curs:
+            drop_db(curs)
+            create_db(curs)
+            add_client(curs, "Михаил", "Шабалин", "712qfd@gmail.com")
+            add_client(curs, "Кирилл", "Лебедев", "009aaf@gmail.com")
+            curs.execute("""
+                SELECT c.id, c.first_name, c.last_name, c.email, p.phone FROM clients c
+                LEFT JOIN phone_numbers p ON c.id = p.client_id
+                ORDER BY c.id
+            """)
+            print(curs.fetchall())
+            add_phone(curs, 1, 79163452130)
+            curs.execute("""
+                        SELECT c.id, c.first_name, c.last_name, c.email, p.phone FROM clients c
+                        LEFT JOIN phone_numbers p ON c.id = p.client_id
+                        ORDER BY c.id
                     """)
-        print(curs.fetchall())
-        change_client(curs, 1, "Иван", "Труд", "klijh54@gmail.com")
-        curs.execute("""
-                    SELECT c.id, c.first_name, c.last_name, c.email, p.phone FROM clients c
-                    LEFT JOIN phone_numbers p ON c.id = p.client_id
-                    ORDER BY c.id
-                    """)
-        print(curs.fetchone())
-        delete_phone(curs, 1)
-        curs.execute("""
-                    SELECT c.id, c.first_name, c.last_name, c.email, p.phone FROM clients c
-                    LEFT JOIN phone_numbers p ON c.id = p.client_id
-                    ORDER BY c.id
-                    """)
-        print(curs.fetchone())
-        delete_client(curs, 1)
-        curs.execute("""
-                    SELECT c.id, c.first_name, c.last_name, c.email, p.phone FROM clients c
-                    LEFT JOIN phone_numbers p ON c.id = p.client_id
-                    ORDER BY c.id
-                    """)
-        print(curs.fetchall())
-        print(find_client(curs, 'Кирилл'))
-conn.close()
+            print(curs.fetchall())
+            print(find_client(curs, None, None, None, '79163452130'))
+            change_client(curs, 1, "Иван", None, "klijh54@gmail.com")
+            curs.execute("""
+                        SELECT c.id, c.first_name, c.last_name, c.email, p.phone FROM clients c
+                        LEFT JOIN phone_numbers p ON c.id = p.client_id
+                        ORDER BY c.id
+                        """)
+            print(curs.fetchone())
+            delete_phone(curs, 1)
+            curs.execute("""
+                        SELECT c.id, c.first_name, c.last_name, c.email, p.phone FROM clients c
+                        LEFT JOIN phone_numbers p ON c.id = p.client_id
+                        ORDER BY c.id
+                        """)
+            print(curs.fetchone())
+            delete_client(curs, 1)
+            curs.execute("""
+                        SELECT c.id, c.first_name, c.last_name, c.email, p.phone FROM clients c
+                        LEFT JOIN phone_numbers p ON c.id = p.client_id
+                        ORDER BY c.id
+                        """)
+            print(curs.fetchall())
+            print(find_client(curs, None, None, '009aaf@gmail.com'))
+            print(find_client(curs, 'Кирилл'))
+    conn.close()
